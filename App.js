4ce6c6036f0f600e746778b2f94c8ef60f585e34
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-02-18 19:26:14 +0530

    add navigation

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text, View } from 'react-native';
+// import { Text, View } from 'react-native';
 import Home from './screens/Home';
+
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={{
-        flex: 1,
-      }}>
-      <Home/>
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>                                                                                                
   );
 }
-
+                                                                  
 export default App;
--- components/List.js
+++ components/List.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions, FlatList, Text} from 'react-native'
+import { View, StyleSheet, FlatList, Text} from 'react-native'
 import Card from './Card';
 import PropTypes from 'prop-types';
 
 const propTypes = {
     title: PropTypes.string,
-    content: PropTypes.object
+    content: PropTypes.array,
 };
 class List extends React.PureComponent {
     render() {
